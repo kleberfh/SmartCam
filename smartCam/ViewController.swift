a7955a0bfd5fdd72f58f0fbcf5bf16f47f512747
Kleber Fernando <kleber.fernando@gruponewway.com.br>
2018-07-31 14:20:04 -0300

    Initial Commit

---
+++ smartCam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  smartCam
+//
+//  Created by Kleber Fernando on 31/07/18.
+//  Copyright © 2018 Kleber Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
